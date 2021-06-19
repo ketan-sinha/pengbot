4258fe8998bb4f7d4bce0476bc789c94206a0075
Ketan Sinha <ktn1997@gmail.com>
2021-06-19 01:54:28 -0500

    Initial command handler

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
-const Discord = require('discord.js')
+const { Client, Intents, Collection } = require('discord.js');
 const {
-  prefix,
-  token
-} = require('./config.json')
+  guild_id,
+  token,
+} = require('./config.json');
 
-const client = new Discord.Client();
-client.login(token)
+const fs = require('fs');
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+const client = new Client({ intents: Intents.FLAGS.GUILD_MESSAGES });
+client.commands = new Collection();
+
+client.on('ready', async () => {
+  console.log('🐧🤖 is ready!');
+
+  for (const file of commandFiles) {
+    const cmd = require(`./commands/${file}`);
+    const data = {
+      name: cmd.name,
+      description: cmd.description,
+      options: cmd.options,
+    };
+    await client.guilds.cache.get(guild_id).commands.create(data);
+    client.commands.set(cmd.name, cmd);
+  }
+});
+
+client.on('interaction', async interaction => {
+  const command = interaction.commandName;
+
+  if (!interaction.isCommand()) return;
+  if (!client.commands.get(command)) return;
+
+  try {
+    await client.commands.get(command).execute(interaction);
+  }
+  catch (e) {
+    console.error(e);
+    interaction.reply('There was an error executing that command!');
+  }
+});
+
+client.login(token);
