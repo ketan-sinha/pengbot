b8a669527e199a174b9d76fa183f6c0ac500e1e0
Ketan Sinha <ktn1997@gmail.com>
2021-06-23 14:30:53 -0500

    Added quote create command

--- commands/quote.js
+++ commands/quote.js
@@ -1,5 +1,7 @@
 const { MessageAttachment } = require('discord.js');
+const mongo = require('../mongo/mongo');
 const { quotes } = require('../resources/txt/quotes');
+const quotesSchema = require('../mongo/schemas/quotes');
 
 module.exports = {
   name: 'quote',
@@ -10,6 +12,31 @@ module.exports = {
       'description': 'Have Pengwing say a random quote',
       'type': 'SUB_COMMAND',
       'required': false,
+    },
+    {
+      'name': 'add',
+      'description': 'Add a new quote for Pengwing to say',
+      'type': 'SUB_COMMAND',
+      'options': [
+        {
+          'name': 'quote',
+          'description': 'The quote',
+          'type': 'STRING',
+          'required': true,
+        },
+        {
+          'name': 'author',
+          'description': 'Who said the quote?',
+          'type': 'USER',
+          'required': true,
+        },
+        {
+          'name': 'context',
+          'description': 'The context of the quote',
+          'type': 'STRING',
+          'required': false,
+        },
+      ],
     },
   ],
   async execute(interaction) {
@@ -24,6 +51,27 @@ module.exports = {
         },
       };
       await interaction.reply({ embeds: [embed], files: [img] });
+    }
+    else if (interaction.options.has('add')) {
+      const options = interaction.options.get('add').options;
+
+      const quote = options.get('quote').value;
+      const author = options.get('author').value;
+      const context = options.get('context').value;
+
+      await interaction.reply(`quote added: ${quote}, by ${author}, with context: ${context}`);
+      await mongo().then(async (mongoose) => {
+        try {
+          await new quotesSchema({
+            quote: quote,
+            author: author,
+            context: context,
+          }).save();
+        }
+        finally {
+          mongoose.connection.close();
+        }
+      });
     }
   },
 };
